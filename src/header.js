8f271272876639028a5b7e31cecd575041055623
Your Name <ganeshagale1701@gmail.com>
2022-06-06 00:00:58 +0530

    changes

--- src/header.js
+++ src/header.js
@@ -3,10 +3,8 @@ import "./CSS/header.css"
 
 export default function Header() {
   return (
-<<<<<<< HEAD
-    <div className='header'> <h1>Vandana Misal and south indian</h1></div>
-=======
+
+
     <div className='header'> <h1>Indian Tadka</h1></div>
->>>>>>> sameer
   )
 }
