3f058f0e390faab4a6431d75100c9e73e68bdab4
Ganesh7001 <105035271+Ganesh7001@users.noreply.github.com>
2022-06-05 22:48:24 +0530

    header changed

--- src/header.js
+++ src/header.js
@@ -3,6 +3,6 @@ import "./CSS/header.css"
 
 export default function Header() {
   return (
-    <div className='header'> <h1>Vandana Hotel</h1></div>
+    <div className='header'> <h1>Vandana Misal and south indian</h1></div>
   )
 }
